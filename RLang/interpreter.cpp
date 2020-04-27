cffb7bed761a663db5c3c9c8cfdb019b0546bea1
ravi verma <ravivermaj488@gmail.com>
2020-04-27 22:58:05 +0530

    Declaring Variable Printing Variable supported Only Int

--- RLang/interpreter.cpp
+++ RLang/interpreter.cpp
@@ -2,34 +2,82 @@
 #include<sstream>
 #include<vector>
 
-void rlang::print(int value,bool newline)
+using namespace rlang;
+
+Variable::Variable():
+	m_name(),m_type(),m_int(nullptr),m_float(nullptr),m_string(),m_bool(false)
 {
-	std::cout << value;
-	if (newline)
-	{
-		std::cout << std::endl;
-	}
+
+}
+
+Variable::Variable(std::string name,int value):
+	m_name(name), m_type("int"), m_int (new int), m_float(nullptr), m_string(), m_bool(false)
+{
+	*(m_int) = value;
+}
+
+std::string Variable::Type()
+{
+	return m_type;
+}
+
+std::string Variable::Name()
+{
+	return m_name;
+}
+
+int Variable::Int()
+{
+	return *(m_int);
+}
+
+float Variable::Float()
+{
+	return *(m_float);
+}
+
+std::string Variable::String()
+{
+	return *(m_string);
+}
+
+void Variable::Input(int value)
+{
+
+}
+
+void Variable::Input(float value)
+{
+
+}
+
+void Variable::Input(std::string value)
+{
+
 }
 
 
+rlang::System rlang::System::instance;
+
 void rlang::Execute(Expression e,bool status)
 {
+	if (e.m_tvalue[0].token() == "int")
+	{
+		if (e.m_tvalue[1].Type() == "identifier")
+		{
+			if (e.m_tvalue[2].token() == ";")
+			{
+				Variable var(e.m_tvalue[1].token(),10);
+				System::IO().Allocate(var);
+			}
+		}
+	}
 	if (e.m_tvalue[0].token() == "print")
 	{
-		if (e.m_tvalue.size() == 5)
+		if (e.m_tvalue[2].Type() == "identifier")
 		{
-			std::stringstream s(e.m_tvalue[2].token());
-			int temp_int;
-			s >> temp_int;
-			rlang::print(temp_int,false);
-		}
-		else if (e.m_tvalue.size() == 7)
-		{
-			std::stringstream s(e.m_tvalue[2].token());
-			int temp_int;
-			s >> temp_int;
-			rlang::print(temp_int,true);
-		}
+			System::IO().Print(e.m_tvalue[2].token(), false);
+	    }
 	}
 	else if (e.m_tvalue[0].token() == "exit")
 	{
--- RLang/interpreter.h
+++ RLang/interpreter.h
@@ -4,15 +4,96 @@
 
 namespace rlang
 {
-
 	class Variable
 	{
 	private:
+		std::string m_name;
+		std::string m_type;
+		int* m_int;
+		float* m_float;
+		std::string* m_string;
+		bool m_bool;
 	public:
+		Variable();
+		Variable(std::string name, int value);
+		std::string Type();
+		std::string Name();
+		int Int();
+		float Float();
+		std::string String();
+		void Input(int value);
+		void Input(float value);
+		void Input(std::string value);
 	};
 
-	//std::vector<Variable> var_table;
-	void print(int value, bool newline);
+	class System
+	{
+	private:
+		static System instance;
+		System() {};
+		System(System&) = delete;
+		std::vector<Variable> var_table;
+	public:
+		static System& IO()
+		{
+			return instance;
+		}
+
+		void Allocate(Variable var)
+		{
+			var_table.push_back(var);
+		}
+
+		void Print(std::string name, bool newline)
+		{
+			for (int i = 0; i < var_table.size(); i++)
+			{
+				if (var_table[i].Name() == name)
+				{
+					if (var_table[i].Type() == "int")
+					{
+						std::cout << var_table[i].Int();
+						if (newline)
+						{
+								std::cout << std::endl;
+						}
+					}
+				}
+			}
+		}
+
+		void Print(int value, bool newline)
+		{
+			std::cout << value;
+			if (newline)
+			{
+				std::cout << std::endl;
+			}
+		}
+
+		void Input(Variable& value)
+		{
+			if (value.Type() == "int")
+			{
+				int temp_int;
+				std::cin >> temp_int;
+				value.Input(temp_int);
+			}
+			else if (value.Type() == "float")
+			{
+				float temp_float;
+				std::cin >> temp_float;
+				value.Input(temp_float);
+			}
+			else if(value.Type() == "string")
+			{
+				std::string temp_string;
+				std::cin >> temp_string;
+				value.Input(temp_string);
+			}
+		}
+	};
+
 	void Execute(Expression e, bool status);
 	void Interpreter(std::vector<rlang::Expression>& statement);
 }
